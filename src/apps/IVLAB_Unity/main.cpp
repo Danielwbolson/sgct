f4de7b9f3138ef4f6a2e91784d761cabbab2f975
Danielwbolson <danielwbolson@gmail.com>
2019-07-18 15:11:57 -0500

    Adding some mip-map stuff to try and fix problem

--- src/apps/IVLAB_Unity/main.cpp
+++ src/apps/IVLAB_Unity/main.cpp
@@ -122,6 +122,8 @@ void init() {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
 
 	// Render settings for Alpha in images
 	glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
